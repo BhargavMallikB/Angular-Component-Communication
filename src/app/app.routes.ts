34974e781eda8fc55c7e49a38ba71964907f008a
BhargavMallikB <bhargavmallikb@gmail.com>
2025-03-25 20:30:41 +0530

    chore: add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { InputComponent } from './components/input/input.component';
+import { OutputComponent } from './components/output/output.component';
+import { ViewChildComponent } from './components/view-child/view-child.component';
+import { NgContentComponent } from './components/ng-content/ng-content.component';
+import { ServiceComponent } from './components/service/service.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'input', component: InputComponent },
+    { path: 'output', component: OutputComponent },
+    { path: 'view-child', component: ViewChildComponent },
+    { path: 'ng-content', component: NgContentComponent },
+    { path: 'service', component: ServiceComponent }
+];
