c43826301137af1d36d056f081a93ebe9ca1e343
BhargavMallikB <bhargavmallikb@gmail.com>
2025-03-27 17:16:06 +0530

    feat: add code and comments

--- src/app/components/ng-content/ng-content.component.ts
+++ src/app/components/ng-content/ng-content.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { CommunicationService } from '../../services/communication.service';
+import { Hero } from '../../types/Hero';
+import { ChildComponent } from '../child/child.component';
 
 @Component({
   selector: 'app-ng-content',
   standalone: true,
-  imports: [],
+  imports: [ChildComponent],
   templateUrl: './ng-content.component.html',
   styleUrl: './ng-content.component.css'
 })
 export class NgContentComponent {
 
+  // heroes variable stores the heroes array of Hero Objects
+  heroes: Hero[] = [];
+
+  constructor(private communicationService: CommunicationService){ // Communication Service Dependency Injecting
+    this.heroes = this.communicationService.getHeroes(); // extracting heroes array of objects and storing in heroes variable
+  }
 }
