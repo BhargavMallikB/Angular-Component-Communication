a4d10ec5dfb09291fed14df97372c3a018b9f871
BhargavMallikB <bhargavmallikb@gmail.com>
2025-03-27 17:51:08 +0530

    feat: add code and comments

--- src/app/components/service/service.component.ts
+++ src/app/components/service/service.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CommunicationService } from '../../services/communication.service';
+import { Hero } from '../../types/Hero';
 
 @Component({
   selector: 'app-service',
@@ -9,4 +11,18 @@ import { Component } from '@angular/core';
 })
 export class ServiceComponent {
 
+  avengers: Hero[] = []; // avengers stores the heroes data coming from CommunicationService
+
+  service: any; //
+
+  constructor(private communicationService: CommunicationService){} // CommunicationService Dependency Injected
+
+  ngOnInit(){
+    this.avengers = this.communicationService.getHeroes(); // avengers stores the data extracts from CommunicationService
+    
+  }
+
+  updateAvengersStatus(hero: {title: string, quote: string, imageURL: string, isActive: boolean}){
+    hero.isActive = !hero.isActive; // It changes the particular hero status to active or deactive
+  }
 }
