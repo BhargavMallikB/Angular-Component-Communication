e6fb0ad51fed51fe0ff2d554cb828f843dbf21f9
BhargavMallikB <bhargavmallikb@gmail.com>
2025-03-27 16:34:23 +0530

    feat: add code and comments

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { CommunicationService } from '../../services/communication.service';
+import { ChildComponent } from '../child/child.component';
 
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [],
+  imports: [ChildComponent],
   templateUrl: './input.component.html',
   styleUrl: './input.component.css'
 })
 export class InputComponent {
 
+  // It stores the all hero names comes from the communication service
+  heroNames: string[] = [];
+  
+  constructor(private communicationService: CommunicationService){ // Dependency Injection of CommunicationService
+    this.heroNames = this.communicationService.getHeroNames(); //retrieving an storing heroes names
+  }
 }
