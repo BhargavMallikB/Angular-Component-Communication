d025119430e1b7eaa7ca97ed871a9df7047f7dbb
BhargavMallikB <bhargavmallikb@gmail.com>
2025-03-27 17:57:32 +0530

    feat: add code and comments

--- src/app/components/child/child.component.ts
+++ src/app/components/child/child.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommunicationService } from '../../services/communication.service';
 
 @Component({
   selector: 'app-child',
@@ -8,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrl: './child.component.css'
 })
 export class ChildComponent {
+  
+  heroQuotes: string[] = []; // stores heroes quotes in this, data gets from CommunicationService
+  heroImages: string[] = []; // stores heroes ImagURls in this, data gets from CommunicationService
 
+  // names will extract the data coming from Parent Component called Input using input() decorator and stores
+  @Input() names: string[] = [];
+
+  // hQuotes will create EventEmitter instance to emit data from here to Parent Component
+  @Output() hQuotes = new EventEmitter<string[]>();
+
+  constructor(private communicationService: CommunicationService){ // CommunicationService Dependency Injected
+    this.heroQuotes = this.communicationService.getHeroQuotes(); // extracts & stors data from service
+    this.heroImages = this.communicationService.getHeroImages(); // extracts & stores data from service
+  }
+
+  ngOnInit(){
+    this.hQuotes.emit(this.heroQuotes); // this emits the data to Parent Component after compoent initialized
+  }
+
+  getImages(){ // getImages returns the heroes ImageURLs to the Parent Component
+    return this.heroImages;
+  }
 }
