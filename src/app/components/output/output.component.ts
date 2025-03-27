6f433084dbc2158ffc76866cd609ed82d463c36e
BhargavMallikB <bhargavmallikb@gmail.com>
2025-03-27 16:44:52 +0530

    feat: add code and comments

--- src/app/components/output/output.component.ts
+++ src/app/components/output/output.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { ChildComponent } from '../child/child.component';
 
 @Component({
   selector: 'app-output',
   standalone: true,
-  imports: [],
+  imports: [ChildComponent],
   templateUrl: './output.component.html',
   styleUrl: './output.component.css'
 })
 export class OutputComponent {
-
+  // quotes receive the data and stores coming from Child Component called Child
+  quotes: string[] =[];
 }
