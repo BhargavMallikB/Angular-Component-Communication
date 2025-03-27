00c76abd619183fd338577f79a0182df292f514d
BhargavMallikB <bhargavmallikb@gmail.com>
2025-03-27 17:01:26 +0530

    feat: add code and comments

--- src/app/components/view-child/view-child.component.ts
+++ src/app/components/view-child/view-child.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ChildComponent } from '../child/child.component';
 
 @Component({
   selector: 'app-view-child',
   standalone: true,
-  imports: [],
+  imports: [ChildComponent],
   templateUrl: './view-child.component.html',
   styleUrl: './view-child.component.css'
 })
 export class ViewChildComponent {
 
+  // images variable stores the data extracts from child component
+  images: string[] = [];
+
+  // @ViewChild() gets the full access on Child Component and stores in a variable 
+  @ViewChild(ChildComponent) data !: ChildComponent;
+  
+  // ngAfterViewInit() access the child component data and stores in images variable
+  ngAfterViewInit(){
+    this.images = this.data.getImages();
+  }
 }
